65fae97dba80730f7cfb615ed26da137f5f31995
Sidney Silva <sidneyolimpio01@gmail.com>
2025-10-20 23:03:48 -0300

    Delete DevBurguer directory

--- DevBurguer/products.js
+++
@@ -1,8 +0,0 @@
-const menuOptions = [
-    { name: "X-Salada", price: 30, vegan: false, src: "/imgs/xsalada.jpeg" },
-    { name: "X-Bacon", price: 34, vegan: false, src: "/imgs/xbacon.jpg" },
-    { name: "X-Bacon Egg", price: 39, vegan: false, src: "/imgs/bacon-egg.jpg" },
-    { name: "Monstruoso", price: 50, vegan: false, src: "/imgs/monstruoso.jpg" },
-    { name: "Big Vegano", price: 55, vegan: true, src: "/imgs/xvegan.jpg" },
-    { name: "X-Vegan", price: 45, vegan: true, src: "/imgs/monstruoso-vegan.jpg" },
-];
